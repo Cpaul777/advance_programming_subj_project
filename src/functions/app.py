19c115fd9840f462f9fbe1725244e7aa9276d68c
Cpaul777 <sabanalpaul009@gmail.com>
2025-11-30 02:39:43 +0800

    Updated the files for less confusion

--- src/functions/app.py
+++ src/functions/app.py
@@ -5,12 +5,16 @@ app = ctk.CTk()
 app.title("App Banking System")
 app.geometry("500x300")
 
+# Just for testing purposes
 app.ada = ctk.CTkButton(app, text="DID THIS WORK?")
 app.ada.pack(pady=50)
 
+# Confirmation Buttons
 buttons.confirm_yes(app)
 buttons.confirm_no(app)
 
+# Action Buttons
+# buttons.back_button(app)
 
 
 
--- src/widgets/buttons.py
+++ src/widgets/buttons.py
@@ -1,30 +1,6 @@
 import customtkinter
 from src.style import dimensions
 
-# Confirmation buttons
-# class confirmationButtons:
-#     def __init__(self, master):
-#         self.master = master
-#         self.create_buttons()
-
-#     def create_buttons(self):
-#         self.confirm_yes = customtkinter.CTkButton(
-#             master=self.master,
-#             text="Y",
-#             width=dimensions.buttonWidth,
-#             height=dimensions.buttonHeight,
-#         )
-
-#         self.confirm_no = customtkinter.CTkButton(
-#             master=self.master,
-#             text="N",
-#             width=dimensions.buttonWidth,
-#             height=dimensions.buttonHeight,
-#         )
-
-#         self.confirm_yes.pack(pady = 5)
-#         self.confirm_no.pack(pady = 10)
-
 def confirm_yes(master):
     master.confirm_yes = customtkinter.CTkButton(
         master=master,
@@ -52,8 +28,3 @@ def confirm_no(master):
 
 # )
 
-# def button_event():
-#     print('button pressed')
-
-# deposit = customtkinter.CTkButton(app, text='CTkButton', width=140, height=28, command=button_event)
-# deposit.place(x=10, y=10)
