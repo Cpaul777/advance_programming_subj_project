079bc6c902ce70cf6e9ce381ac5a8daf4f5e9d4b
Cpaul777 <sabanalpaul009@gmail.com>
2025-11-30 02:42:52 +0800

    Updated gitignore and main.py forgot to include from previous commit

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
+from src.functions.app import app
 
-import customtkinter as ctk
-
-app = ctk.CTk()
-app.title("App Banking System")
-app.geometry("400x300")
-
-button = ctk.CTkButton(app, text="click me")
-button.pack()
-
-if __name__ == "__main__":
+if __name__=="__main__":
     app.mainloop()
